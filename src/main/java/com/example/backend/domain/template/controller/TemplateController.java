079e1116da2e4eb53f9ca1a0eb900080a7e7ab61
ujin302 <ujin302@gmail.com>
2025-01-26 17:57:36 +0900

    test

--- src/main/java/com/example/backend/domain/template/controller/TemplateController.java
+++ src/main/java/com/example/backend/domain/template/controller/TemplateController.java
@@ -1,12 +1,20 @@
 package com.example.backend.domain.template.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.domain.template.application.TemplateService;
 
-@RestController
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Tag(name = "Sqagger API", description = "templates")
+// @RestController
+@Controller
 @RequestMapping("api/templates")
 public class TemplateController {
     /*
@@ -16,4 +24,11 @@ public class TemplateController {
 
     @Autowired
     private TemplateService templateService;
+
+    @Operation(summary = "템플릿 목록 출력", description = "메인")
+    @GetMapping("")
+    public String getMethodName() {
+        return "hi";
+    }
+
 }
--- src/main/java/com/example/backend/global/config/SecurityConfig.java
+++ src/main/java/com/example/backend/global/config/SecurityConfig.java
@@ -1,8 +1,31 @@
 package com.example.backend.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+
+        return http.build();
+    }
 
 }
